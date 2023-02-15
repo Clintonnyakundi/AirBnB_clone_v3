fcbfb6d97c45c36f3bc454afa8f8d3764f810162
queensk <queenskisivuli@gmail.com>
2023-02-16 00:08:19 +0300

    retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,17 @@ Returns JSON status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
+
+
+hbnb_objects = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route("/status", methods=['GET'])
@@ -11,3 +22,11 @@ def status():
     """Returns a JSON"""
     if request.method == 'GET':
         return (jsonify({"status": "OK"}))
+
+@app_views.route("/stats", methods=["GET"])
+def hbnbStats():
+    """Retrieves the number of each objects"""
+    return_dict = {}
+    for key, value in hbnb_objects.items():
+        return_dict[key] = storage.count(value)
+    return jsonify({return_dict})
