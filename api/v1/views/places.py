39049be90af380fca6018ba939794cc1697bc006
queensk <queenskisivuli@gmail.com>
2023-02-17 05:09:10 +0300

    create a new place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,8 @@ Create a new view for Place objects that handles all default RESTFul API actions
 """
 from api.v1.views import app_views
 from models import storage
-from flask import abort, jsonify
+from flask import abort, jsonify, request, make_response
+from models.place import Place
 
 
 @app_views.route('/cities/<string:city_id>/places', methods=['GET'],
@@ -38,3 +39,26 @@ def delete_place(place_id):
     place.delete()
     storage.save()
     return (jsonify({}))
+
+
+@app_views.route('/cities/<string:city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def post_place(city_id):
+    """Create a new place"""
+    city = storage.get("City", city_id)
+    new_place = request.get_json()
+    if city is None:
+        abort(404)
+    if not new_place:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'user_id' not in new_place:
+        return make_response(jsonify({'error': 'Missing user_id'}), 400)
+    user = storage.get("User", new_place['user_id'])
+    if user is None:
+        abort(404)
+    if 'name' not in new_place:
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    new_place['city_id'] = city_id
+    place = Place(**new_place)
+    place.save()
+    return make_response(jsonify(place.to_dict()), 201)
