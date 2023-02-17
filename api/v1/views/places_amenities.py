b9795596758db4c608dc8769112e622979b88293
queensk <queenskisivuli@gmail.com>
2023-02-17 07:02:35 +0300

    adds amenity data to a place

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -4,7 +4,7 @@ Create a new view for the link between Place objects and Amenity
 """
 
 from api.v1.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, make_response
 import os
 from models import storage
 
@@ -35,3 +35,19 @@ def delete_place_amenity(place_id, amenity_id):
     place_amenities_ids.remove(amenity)
     place.save()
     return jsonify({})
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def post_place_amenity(place_id, amenity_id):
+    """adds an amenity object to a place"""
+    place = storage.get("Place", place_id)
+    amenity = storage.get("Amenity", amenity_id)
+    if place is None or amenity is None:
+        abort(404)
+    place_amenities = place.amenity_ids
+    if amenity in place_amenities:
+        return jsonify(amenity.to_dict())
+    place_amenities.append(amenity)
+    place.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
