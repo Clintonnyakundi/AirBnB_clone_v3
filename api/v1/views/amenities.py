605e025828aae127e6321b7d8c18c53cf8020f69
queensk <queenskisivuli@gmail.com>
2023-02-17 04:16:30 +0300

    delete amenity on id

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -22,3 +22,15 @@ def get_amenity(amenity_id):
     if amenity is None:
         abort(404)
     return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes ameniy on amenity_id"""
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return (jsonify({}))
