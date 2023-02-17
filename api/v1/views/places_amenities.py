5246a97f71da7117de8bd273986fdf62f18f1ae8
queensk <queenskisivuli@gmail.com>
2023-02-17 06:55:47 +0300

    Delete amenity from place

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,6 +8,7 @@ from flask import abort, jsonify
 import os
 from models import storage
 
+
 @app_views.route('/places/<string:place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_place_amenities(place_id):
@@ -18,3 +19,19 @@ def get_place_amenities(place_id):
         abort(404)
     amenities = [amenity.to_dict() for amenity in amenities_ids]
     return jsonify(amenities)
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """Deletes an amenity data from a place"""
+    place = storage.get("Place", place_id)
+    amenity = storage.get("Amenity", amenity_id)
+    place_amenities_ids = place.amenity_ids
+    if place is None or amenity is None:
+        abort(404)
+    if amenity not in place_amenities_ids:
+        abort(404)
+    place_amenities_ids.remove(amenity)
+    place.save()
+    return jsonify({})
