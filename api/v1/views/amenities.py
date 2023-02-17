efd4370938e15d8461a41fe77c77d9b73795a3df
queensk <queenskisivuli@gmail.com>
2023-02-17 04:14:03 +0300

    get amenities on id

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,11 +4,21 @@ Amenity views
 """
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, abort
 from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
-    """get all amenities"""
+    """Get all amenities"""
     amenities = [amenity.to_dict() for amenity in storage.all("Amenity").values()]
     return jsonify(amenities)
+
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity(amenity_id):
+    """Get amenity on id"""
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
