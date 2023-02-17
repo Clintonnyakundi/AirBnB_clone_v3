409009b4d9e09908394d5b0812bc2ed4e30d17be
queensk <queenskisivuli@gmail.com>
2023-02-17 04:25:12 +0300

    create a new amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,7 +4,7 @@ Amenity views
 """
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, abort
+from flask import jsonify, abort, request, make_response
 from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
@@ -34,3 +34,16 @@ def delete_amenity(amenity_id):
     amenity.delete()
     storage.save()
     return (jsonify({}))
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+    """Create a new amenity"""
+    new_amenity = request.get_json()
+    if not new_amenity:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in new_amenity:
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    amenity = Amenity(**new_amenity)
+    amenity.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
